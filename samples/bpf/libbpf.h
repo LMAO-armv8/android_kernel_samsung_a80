acc8bcae9dbe64367871f67e7ac17bc504ddc0ef
LMAO-armv8 <chaitanya4g9@sasi.ac.in>
2022-10-17 18:19:22 +0530

    samples: Import OEM Changes
    Signed-off-by: LMAO-armv8 <chaitanya4g9@sasi.ac.in>

--- samples/bpf/libbpf.h
+++ samples/bpf/libbpf.h
@@ -1,7 +1,9 @@
 /* SPDX-License-Identifier: GPL-2.0 */
-/* eBPF instruction mini library */
-#ifndef __BPF_INSN_H
-#define __BPF_INSN_H
+/* eBPF mini library */
+#ifndef __LIBBPF_H
+#define __LIBBPF_H
+
+#include <bpf/bpf.h>
 
 struct bpf_insn;
 
